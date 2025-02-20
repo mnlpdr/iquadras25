95b01fe6dba337ad67d3335b36c8c8804b7c8eba
LuizManoeldev <dantasluizmanoel@gmail.com>
2025-02-20 08:42:53 -0300

    feat: reservations 100%

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,38 +9,37 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
     @courts = Court.all # Carregar todas as quadras disponíveis
   end
-  
+
   def create
     @reservation = Reservation.new(reservation_params)
     @reservation.user = current_user
+    @courts = Court.all
     
     court = @reservation.court
-    start_time = Time.parse(@reservation.start_time)
-    end_time = Time.parse(@reservation.end_time)
+    start_time = @reservation.start_time
+    end_time = @reservation.end_time
     date = @reservation.date
-
+  
     # Validar se a reserva está dentro do horário de funcionamento da quadra
     if !court.working_hours_range.cover?(start_time) || !court.working_hours_range.cover?(end_time)
-      flash.now[:alert] = "A reserva deve estar dentro do horário de funcionamento da quadra."
-      render :new, status: :unprocessable_entity
-      return
+      @reservation.errors.add(:base, "A reserva deve estar dentro do horário de funcionamento da quadra.")
+      render :new, status: :unprocessable_entity and return
     end
-
+  
     # Verificar se já existe uma reserva no mesmo horário e data para a mesma quadra
     if Reservation.exists?(court_id: court.id, date: date, start_time: start_time..end_time)
-      flash.now[:alert] = "Já existe uma reserva no mesmo horário para essa quadra."
-      render :new, status: :unprocessable_entity
-      return
+      @reservation.errors.add(:base, "Já existe uma reserva no mesmo horário para essa quadra.")
+      render :new, status: :unprocessable_entity and return
     end
-
+  
     # Salvar a reserva
     if @reservation.save
       redirect_to reservations_path, notice: "Reserva criada com sucesso!"
     else
-      flash.now[:alert] = @reservation.errors.full_messages.join(", ")
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
+  
 
   def destroy
     @reservation = Reservation.find(params[:id])
--- app/models/court.rb
+++ app/models/court.rb
@@ -4,4 +4,8 @@ class Court < ApplicationRecord
     has_many :court_sports
     has_many :sports, through: :court_sports
 
+    def working_hours_range
+        start_hour, end_hour = working_hours.split("-").map { |t| Time.zone.parse(t) }
+        start_hour..end_hour
+      end
 end
